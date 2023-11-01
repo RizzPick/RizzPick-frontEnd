0f9fa36ca56e7cd0cf8a34a1625898010f57d982
seokjin909 <thseokjin@naver.com>
2023-11-01 17:44:02 +0900

    #24 [style] 데스크탑 마이 프로필 상세 페이지 작업중

--- src/components/profile/UserProfile.tsx
+++ src/components/profile/UserProfile.tsx
@@ -1,11 +1,12 @@
 'use client'
 import Image from 'next/image';
 import React, { useState } from 'react'
-import MultiCarousel from '../common/MultiCarousel';
 import { useRouter } from 'next/navigation';
-import LocationIcon from "../../../public/profileIcon/location.svg"
 import EducationIcon from "../../../public/profileIcon/graduationcap.fill.svg"
 import { MyProfileRes } from '@/types/profile';
+import Home from "../../../public/profileIcon/Home.svg"
+import { eraseCookie } from '@/utils/cookie';
+import Link from 'next/link';
 
 
 type Props = {
@@ -13,67 +14,83 @@ type Props = {
 }
 function UserProfile({profile} : Props) {
   const router = useRouter();
-  const [showSecondSection, setShowSecondSection] = useState(false); // 두 번째 section을 보여줄지 결정하는 상태
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
+    const logout = () => {
+        eraseCookie('Authorization');
+        eraseCookie('Authorization_Refresh');
+        eraseCookie('status');
+        sessionStorage.clear();
+        alert('로그아웃 처리되었습니다.');
+        router.push('/');
+    };
 
   const onClick = (event : any) => {
     event.stopPropagation();
     router.push('/profile/edit');
   }
-  const handleIntroClick = () => {
-    setShowSecondSection(prev => !prev);
-}
 
   return (
-    <div className='flex items-center justify-center'>
-      <section className="my-5 w-[487px] sm:w-[90vw]">
-      <MultiCarousel>
-        {profile.profileImages.map((image) => 
-        <article key={image.id} className='rounded-3xl overflow-hidden w-[487px] h-[731px] relative sm:w-full'>
-          <Image src={image.image} alt='프로필 이미지' fill style={{objectFit : 'cover'}} priority className='rounded-md'/>
-          <div onClick={handleIntroClick} className='cursor-pointer flex flex-col p-4 border w-[487px] mx-auto rounded-3xl absolute -bottom-0 bg-white sm:w-full' id='intro'>
-            <div className='flex justify-between w-full'>
-              <h3 className='font-bold text-2xl'>{profile.nickname}</h3>
-              <p className='text-gray-400 text-lg'>{profile.age}</p>
-            </div>
-            <div className='justify-start flex font-bold text-lg text-gray-500'>{profile.intro}</div>
-          <button onClick={onClick} className='mx-auto px-4 py-2 bg-gradient-end mt-4 w-40 rounded-3xl text-white font-bold hover:bg-pink-300'>정보수정</button>
-          </div>
-        </article>)}
-      </MultiCarousel>
-      </section>
-      <section className='flex flex-col h-[731px] ml-20' style={{ display: showSecondSection ? 'block' : 'none' }}>
-      <div className='w-[467px] border bg-[#CACFFF] h-[398px] rounded-3xl mb-3'>
-            <h1 className='text-center text-xl mt-4'>나랑 이런 데이트 어때요</h1>
-            <div className='w-[430px] h-[312px] border bg-white mx-auto rounded-3xl mt-4 p-4 flex items-center justify-center'>
-              {profile.dating && profile.dating.length > 0 ? 
-                  <ul className='list-disc pl-5 space-y-2'>
-                      {profile.dating.map((date) => {
-                          return <li key={date.datingId}>{date.datingTitle}</li>
-                      })}
-                  </ul>
-                  : 
-              <button onClick={()=>router.push('/user/plan/board')} className='mx-auto px-4 py-2 bg-gradient-end mt-4 rounded-3xl text-white font-bold hover:bg-pink-300'>데이트 계획 추가하기</button>
-              }
-          </div>
+    <div className='bg-profile-gradient h-[100vh] py-[67px]'>
+        <div className='relative w-[169px] h-[169px] mx-auto'>
+          <Image src={profile.profileImages[0].image} alt='프로필 이미지' fill style={{objectFit:'cover'}} className='rounded-full'/>
         </div>
-        <div className="flex-1 bg-profile-gradient text-white p-4 rounded-3xl relative h-[315px] w-[467px]" id='options'>
-          <div className='border rounded-3xl bg-white text-gray-500 p-4 h-[281px] flex flex-col gap-4 justify-center'>
-          { !profile.location && !profile.education && !profile.mbti && !profile.religion ? 
-            <p className="text-center">작성된 내용이 없습니다.</p> 
-            : 
-            <>
-              { profile.location ? <div className='flex items-center gap-2'><LocationIcon/>{profile.location}</div> : null }
-              { profile.education ? <div className='flex items-center gap-2'><EducationIcon/>{profile.education}</div> : null }
-              { (profile.mbti || profile.religion) && <hr className='border-dashed'/> }
-              <div className='flex items-center gap-4'>
-                { profile.mbti ? <div className='px-3 py-1 border-[#d67dff] border-2 rounded-3xl'>{profile.mbti}</div> : null }
-                { profile.religion ? <div className='px-3 py-1 border-[#d67dff] border-2 rounded-3xl'>{profile.religion}</div> : null }
+        <section className='flex items-start justify-center flex-row gap-9 mt-[67px]'>
+          <article className='w-[424px] h-[428px]'>
+            <div className='flex flex-col w-full items-center'>
+                <div className='p-4 bg-white h-[200px] w-full rounded-2xl flex flex-col gap-3 justify-center'>
+                    {!profile.location && !profile.education && !profile.mbti && !profile.religion ? 
+                        <p className="text-center">작성된 내용이 없습니다.</p> 
+                        : 
+                        <>
+                            { profile.education ? <div className='flex items-center gap-4 border-b py-2'><EducationIcon/>{profile.education}</div> : null }
+                            { profile.location ? <div className='flex items-center gap-4 border-b py-2'><Home/>{profile.location}</div> : null }
+                            <div className='flex items-center gap-4'>
+                            { profile.mbti ? <div className='px-3 py-1 border-fuchsia-400 border-2 rounded-3xl text-fuchsia-400'>#{profile.mbti}</div> : null }
+                            { profile.religion ? <div className='px-3 py-1 border-fuchsia-400 border-2 rounded-3xl text-fuchsia-400'>#{profile.religion}</div> : null }
+                            </div>
+                        </>
+                    }
+                </div>
+                  <Link href={'/profile/edit'} className='w-full'>
+                      <button className='px-4 py-2 bg-[#D67dff] mt-4 w-full rounded-3xl text-white font-bold hover:bg-pink-300'>정보수정</button>
+                  </Link>
+                <div className='mt-4 w-full rounded-2xl bg-white p-6 h-20 flex flex-col items-start justify-center text-gray-400 gap-1'>
+                    <p className='cursor-pointer' onClick={()=>router.push('/')}>비밀번호 변경</p>
+                    <hr className='w-full'/>
+                    <p onClick={()=>setShowLogoutModal(true)} className='cursor-pointer'>로그아웃</p>
+                </div>
+
+                {showLogoutModal && (
+                    <div className="fixed inset-0 flex items-center justify-center z-50">
+                        {/* 모달 외부 배경 (그레이 오버레이) */}
+                        <div className="absolute inset-0 bg-gray-500 opacity-50" onClick={() => setShowLogoutModal(false)}></div>
+                        
+                        {/* 모달 창 */}
+                        <div className="bg-white p-2 rounded-xl shadow-lg w-64 z-10 h-32 flex flex-col items-center justify-center">
+                            <div>로그아웃 할까요?</div>
+                            <div className="flex justify-between mt-4 w-full px-6">
+                                <button onClick={() => setShowLogoutModal(false)} className="mr-2 px-4 py-2 rounded">취소</button>
+                                <button onClick={() => { logout(); setShowLogoutModal(false); }} className="px-4 py-2rounded-lg transition-all hover:scale-125">확인</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+                <div className='mt-4 w-full rounded-2xl bg-white p-6 h-10 flex flex-col items-start justify-center text-[#cb17f9]'>
+                    회원탈퇴
+                </div>
               </div>
-            </>
-          }
-        </div>
-      </div>
-      </section>
+              </article>
+              <article>
+                <div className='grid grid-cols-3 gap-4'>
+                    <div className='w-[183px] h-[221px] border bg-white'></div>
+                    <div className='w-[183px] h-[221px] border bg-white'></div>
+                    <div className='w-[183px] h-[221px] border bg-white'></div>
+                    <div className='w-[183px] h-[221px] border bg-white'></div>
+                    <div className='w-[183px] h-[221px] border bg-white'></div>
+                    <div className='w-[183px] h-[221px] border bg-white'></div>
+                </div>
+              </article>
+          </section>
   </div>
   )
 }
