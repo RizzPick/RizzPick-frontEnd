3ca266b803b3288c5e1be0db2ac63e264e1d4b30
seokjin909 <thseokjin@naver.com>
2023-11-11 22:12:58 +0900

    layout props 타입 수정

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,10 +1,14 @@
 'use client'
 import Header from '@/components/common/Header';
+import { ReactNode } from 'react';
+
+interface UserLayoutProps {
+    children: ReactNode;
+    showHeader?: boolean; // showHeader는 선택적 프로퍼티입니다.
+  }
 
 export default function UserLayout({
-    children, showHeader = false}: {
-    children: React.ReactNode; showHeader:boolean
-}) {
+    children, showHeader = false}: UserLayoutProps) {
     return (
         <section>
             {showHeader && <Header />}
