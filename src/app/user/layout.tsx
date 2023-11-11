7f0b32b0e94e16b7ed057509374189502aef3e5b
seokjin909 <thseokjin@naver.com>
2023-11-11 22:23:59 +0900

    [fix] layout export module issue

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -2,17 +2,16 @@
 import Header from '@/components/common/Header';
 import { ReactNode } from 'react';
 
-interface UserLayoutProps {
+type UserLayoutProps  = {
     children: ReactNode;
-    showHeader?: boolean; // showHeader는 선택적 프로퍼티입니다.
+    showHeader?: boolean;
   }
 
-export default function UserLayout({
-    children, showHeader = false}: UserLayoutProps) {
+export default function UserLayout({children, showHeader = false}: UserLayoutProps) {
     return (
-        <section>
+        <>
             {showHeader && <Header />}
             {children}
-        </section>
+        </>
     );
 }
