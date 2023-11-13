6f79ecea0ab9f67f37201c91ac1a33cf18acd3f0
Xeonxoo_99 <dustn49@gmail.com>
2023-11-13 23:22:39 +0900

    #17 [fix] 실시간 모달 수정 및 유저 데이트 계획에 이미지 처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
         <html lang="ko">
             <head>
                 <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1.0, user-scalable=yes"
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, user-scalable=yes"
                 />
                 <meta name="msapplication-tap-highlight" content="no" />
             </head>
